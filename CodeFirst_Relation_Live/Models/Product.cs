6c1742024266fd4e858a830490a4ddad0e5bc59f
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-05-15 12:04:10 +0300

    Some changes

--- CodeFirst_Relation_Live/Models/Product.cs
+++ CodeFirst_Relation_Live/Models/Product.cs
@@ -15,6 +15,7 @@ namespace CodeFirst_Relation_Live.Models
 
         // Burada da Product tablomun Order tablosunu beslediğini belirten tanımlar yapmam lazım
 
+
         public ICollection<Order> Orders { get; set; }
     }
 }
